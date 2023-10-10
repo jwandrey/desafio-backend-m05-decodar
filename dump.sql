4436b7484bcd8533af9139c10b56d4575ea2bb4f
Geana Mossal <geana.almeida2000@gmail.com>
2023-10-10 12:05:10 -0300

    Adicionado o arquivo dump.sql

---
+++ dump.sql
@@ -0,0 +1,28 @@
+                            --Criação do banco de dados--
+create database pdv;
+
+                            --Criação das tabelas--
+create table usuarios (
+  id serial primary key,
+  nome text not null,
+  email text not null unique,
+  senha text not null
+ );
+
+create table categorias (
+  id serial primary key,
+  descricao text not null
+ );
+
+                            --Inserindo as categorias--
+insert into categorias (descricao) values 
+  ('Informática')
+  ('Celulares'),
+  ('Beleza e Perfumaria'),
+  ('Mercado'),
+  ('Livros e Papelaria'),
+  ('Brinquedos'),
+  ('Moda'),
+  ('Bebê'),
+  ('Games');
+
