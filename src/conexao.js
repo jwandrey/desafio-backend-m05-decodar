b3766e2bc98b8c6116e34c468a07df8feb37cca3
Darllisouza <darllianedebora@gmail.com>
2023-10-12 11:22:33 -0300

    aleatorio

--- src/conexao.js
+++ src/conexao.js
@@ -5,8 +5,9 @@ const knex = require('knex')({
     port : process.env.DB_PORT, 
     user : process.env.DB_USER, 
     password : process.env.DB_PASSWORD, 
-    database : process.env.DB_NAME
+    database : process.env.DB_NAME,
     },
 });
 
 module.exports = knex;
+
