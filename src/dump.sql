01662c9deb07e4c7433557747bb96c379044b005
Geana Mossal <geana.almeida2000@gmail.com>
2023-10-18 12:51:59 -0300

    Adicionado a segunda parte da sprint do banco de dados

--- src/dump.sql
+++ src/dump.sql
@@ -20,3 +20,25 @@ insert into categorias (descricao) values
   ('Moda'),
   ('Bebê'),
   ('Games');
+
+                              --Segunda sprint
+create table produtos (
+  id serial primary key,
+  descricao text not null,
+  quantidade_estoque integer not null,
+  valor decimal not null,
+  categoria_id integer not null references categorias(id)
+ );
+
+ create table clientes (
+ 	 id serial primary key,
+   nome text not null,
+   email text not null unique,
+   cpf text not null unique,
+   cep text not null,
+   rua text not null,
+   numero text not null,
+   bairro text not null,
+   cidade text not null,
+   estado text not null
+ );
