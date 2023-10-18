13c2f775a32c982f76b05c9c2fef3fd79c4ab819
jess <jwandreyy@gmail.com>
2023-10-18 16:51:13 -0300

    add(produtos): cadastrarProduto e listarProdutos / att(dump.sql)

--- src/dump.sql
+++ src/dump.sql
@@ -3,12 +3,12 @@ create table usuarios (
   nome text not null,
   email text not null unique,
   senha text not null
- );
+);
 
 create table categorias (
   id serial primary key,
   descricao text not null
- );
+);
 
 insert into categorias (descricao) values 
   ('Informática'),
@@ -21,24 +21,23 @@ insert into categorias (descricao) values
   ('Bebê'),
   ('Games');
 
-                              --Segunda sprint
 create table produtos (
   id serial primary key,
   descricao text not null,
   quantidade_estoque integer not null,
-  valor decimal not null,
+  valor integer not null,
   categoria_id integer not null references categorias(id)
- );
+);
 
- create table clientes (
- 	 id serial primary key,
-   nome text not null,
-   email text not null unique,
-   cpf text not null unique,
-   cep text not null,
-   rua text not null,
-   numero text not null,
-   bairro text not null,
-   cidade text not null,
-   estado text not null
- );
+create table clientes (
+ 	id serial primary key,
+  nome text not null,
+  email text not null unique,
+  cpf text not null unique,
+  cep text,
+  rua text,
+  numero text,
+  bairro text,
+  cidade text,
+  estado text
+);
