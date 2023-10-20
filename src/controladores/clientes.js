8c6745183a7ab1e016f4bcad28acc9e96372acf3
Darllisouza <darllianedebora@gmail.com>
2023-10-19 21:26:00 -0300

    Adicionando as rotas edita e detalhar cliente

--- src/controladores/clientes.js
+++ src/controladores/clientes.js
@@ -1,5 +1,6 @@
 const knex = require('../conexao');
 const { verificaIdExistente } = require('../utils/verificacoes');
+const joi = require("joi"); //usado para validação de dados
 
 const cadastrarCliente = async (req, res) => {
     const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
@@ -13,14 +14,59 @@ const cadastrarCliente = async (req, res) => {
 }
 
 const editarCliente = async (req, res) => {
-    const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
+  const { id } = req.params;
+  const { nome, email, cpf, cep, rua, numero, bairro, cidade, estado } = req.body;
 
-    try {
-        
-     } catch (error) {
-       console.error(error.message);
-       return res.status(500).json({ mensagem: "Erro interno do servidor." });
-     }
+  // Define as regras de validação usando Joi
+  const dadosObrigatorios = joi.object({
+    nome: joi.string().required(), //obrigatorio
+    email: joi.string().email().required(),//obrigatorio
+    cpf: joi.string().length(11).required(),//obrigatorio
+    cep: joi.string().length(8).allow(null), //daqui pra baixo e opcional
+    rua: joi.string().allow(null),
+    numero: joi.string().allow(null),
+    bairro: joi.string().allow(null),
+    cidade: joi.string().allow(null),
+    estado: joi.string().allow(null),
+  });
+
+  try {
+    // Validar os dados recebidos no corpo da requisição
+    const { error } = dadosObrigatorios.validate(req.body); 
+
+    if (error) {
+      return res.status(400).json({ mensagem: error.details[0].message });
+    }
+
+   //Validar se existe cliente para o id enviado como parâmetro na rota.
+    const clienteExistente = await knex("clientes").where({ id }).first();
+
+    if (!clienteExistente) {
+      return res.status(404).json({ mensagem: "Cliente não encontrado" });
+    }
+
+    //não permitindo dois clientes possuírem o mesmo e-mail e o mesmo cpf.
+    const clienteComEmail = await knex("clientes").where({ email }).whereNot({ id }).first();
+    const clienteComCPF = await knex("clientes").where({ cpf }).whereNot({ id }).first();
+
+    if (clienteComEmail) {
+      return res.status(400).json({ mensagem: "E-mail já cadastrado" });
+    }
+
+    if (clienteComCPF) {
+      return res.status(400).json({ mensagem: "CPF já cadastrado" });
+    }
+
+    // Atualiza os dados do cliente no banco de dados
+    await knex("clientes")
+      .where({ id })
+      .update({ nome, email, cpf, cep, rua, numero, bairro, cidade, estado });
+    
+    return res.status(200).json({ mensagem: "Cliente atualizado com sucesso" });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({ mensagem: "Erro interno do servidor." });
+  }
 }
 
 const listarClientes = async (req, res) => {
@@ -34,8 +80,16 @@ const listarClientes = async (req, res) => {
 
 const detalharClientePorId = async (req, res) => {
     try {
+      const { id } = req.params;
+    // Consultar o banco de dados para encontrar o cliente pelo id.
+    const cliente = await knex("clientes").where({ id }).first();
+
+    if (!cliente) {
+      return res.status(404).json({ mensagem: "Cliente não encontrado" });
+    }
+    return res.json(cliente);
         
-     } catch (error) {
+    } catch (error) {
        console.error(error.message);
        return res.status(500).json({ mensagem: "Erro interno do servidor." });
      }
