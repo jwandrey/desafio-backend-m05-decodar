1dfc66c199f490a9105f456fc119fc685354e4c7
jess <jwandreyy@gmail.com>
2023-10-24 18:09:52 -0300

    feat(pedidos): envio de email ao cadastrar pedido com o nodemailer

---
+++ src/servicos/nodemailer.js
@@ -0,0 +1,12 @@
+const nodemailer = require('nodemailer');
+
+const transportador = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+    }
+})
+
+module.exports = transportador;
